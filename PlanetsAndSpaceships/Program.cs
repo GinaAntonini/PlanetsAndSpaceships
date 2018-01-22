f7bff2214dcc3a91c42b8f292995dd0fad313731
Gina <ginamantonini@gmail.com>
2018-01-21 22:50:48 -0600

    added Spacecraft class and dictionary for spacecrafts and lists of planets visited

--- PlanetsAndSpaceships/Program.cs
+++ PlanetsAndSpaceships/Program.cs
@@ -43,10 +43,29 @@ namespace PlanetsAndSpaceships
             {
                 Console.WriteLine(planet);
             }
+
+            var spacecraftDictionary = new Dictionary<string, Spacecraft>
+            {
+                { "firstKey", new Spacecraft { Name = "Maven"} },
+                { "secondKey", new Spacecraft { Name = "Pioneer"} },
+                { "thirdKey", new Spacecraft { Name = "Helios"} },
+                { "fourthKey", new Spacecraft { Name = "Ulysses"} },
+                { "fifthKey", new Spacecraft { Name = "Genesis"} },
+                { "sixthKey", new Spacecraft { Name = "Mariner"} },
+                { "seventhKey", new Spacecraft { Name = "Messenger"} },
+                { "eighthKey", new Spacecraft { Name = "Venera"} },
+                { "ninthKey", new Spacecraft { Name = "Galileo"} },
+                { "tenthKey", new Spacecraft { Name = "Magellan"} },
+            };
         }
     }
 }
 
+//Create another dictionary that will hold the name of a spacecraft that we have launched, and a list of names of the planets that it has visited.
+//Remember that List is a Type just like native types (such as string, int, & bool) and your custom types(such as Movie, Dog, and Food). 
+//They can be passed to anything, like a dictionary, as a Type.
+//Iterate over your list of planets from above, and inside that loop, iterate over the dictionary. 
+//Write to the console, for each planet, which satellites have visited which planet.
 
 
 
