a33aa435cd51cd6d983103298dbd500c723e9284
Gina <ginamantonini@gmail.com>
2018-01-21 22:16:10 -0600

    planets are listed as required by assignment instructions

--- PlanetsAndSpaceships/Program.cs
+++ PlanetsAndSpaceships/Program.cs
@@ -10,19 +10,36 @@ namespace PlanetsAndSpaceships
     {
         static void Main(string[] args)
         {
+            //Add() Jupiter and Saturn at the end of the list.
             List<string> planetList = new List<string>() { "Mercury", "Mars", "Jupiter", "Saturn" };
 
+            //Create another List that contains that last two planet of our solar system.
             List<string> remainingPlanetsList = new List<string>() { "Uranus", "Neptune" };
 
+            //Combine the two lists by using AddRange().
             planetList.AddRange(remainingPlanetsList);
 
+            //Use Insert() to add Earth, and Venus in the correct order.
             planetList.Insert(1, "Venus");
             planetList.Insert(2, "Earth");
+
+            //Use Add() again to add Pluto to the end of the list.
             planetList.Add("Pluto");
 
+            //Now that all the planets are in the list, slice the list using GetRange() in order to extract the rocky planets into a new list called rockyPlanets.
+            string[] rockyPlanets = planetList.GetRange(0, 3).ToArray();
+
+            //Being good amateur astronomers, we know that Pluto is now a dwarf planet, so use the Remove() method to eliminate it from the end of planetList.
+            planetList.Remove("Pluto");
 
             Console.WriteLine();
             foreach (string planet in planetList)
+            {
+                Console.WriteLine(planet);
+            }
+
+            Console.WriteLine();
+            foreach (string planet in rockyPlanets)
             {
                 Console.WriteLine(planet);
             }
@@ -31,5 +48,8 @@ namespace PlanetsAndSpaceships
 }
 
 
-//Now that all the planets are in the list, slice the list using GetRange() in order to extract the rocky planets into a new list called rockyPlanets.
-//Being good amateur astronomers, we know that Pluto is now a dwarf planet, so use the Remove() method to eliminate it from the end of planetList.
+
+
+
+
+
